852b22f06be25bc5dcf756e66d6853714ec45120
BearingMe <gomes.bruno201@gmail.com>
2022-01-29 03:07:33 -0300

    feat: adiciona bibliotecas de segurança

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 
 import * as db from "./db";
 import jobsRoute from "./routes/jobs_route";
@@ -9,9 +13,18 @@ import ensureAuthenticate from "./middlewares/authentication";
 // constants
 const app = express();
 const port = process.env.PORT || 3000;
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 50 // limit each IP to 50 requests per windowMs
+});
 
 // middleware
+app.set("trust proxy", 1);
+
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
 
 // routes
 app.use("/api/v1/jobs", ensureAuthenticate, jobsRoute);
